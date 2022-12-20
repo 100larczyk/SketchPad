c30e4ace7f0a99877755e94496827073ddf5d1de
100larczyk <100larczyk.m@gmail.com>
2022-12-20 14:01:05 +0100

    hover works, button works, but container reacts on hover also but it should't - fix it

--- javascript.js
+++ javascript.js
@@ -1,21 +1,40 @@
 const container = document.getElementById("container");
+const button = document.querySelector("button");
+let div = containerDiv;
 function createDiv() {
   const containerDiv = document.createElement("div");
 
   containerDiv.className = "new-div";
-
   return containerDiv;
 }
 
+let numOfDivs;
+let numbOfDivsMultiplied;
 function createMultipleDivs() {
   let myDivs = [];
   let i = 0;
-  const numOfDivs = 255;
 
-  for (i; i <= numOfDivs; i++) {
+  for (i; i <= numbOfDivsMultiplied; i++) {
     myDivs.push(createDiv());
     container.appendChild(myDivs[i]);
   }
 }
-
 createMultipleDivs();
+
+button.addEventListener("click", gridSizePrompt);
+function gridSizePrompt(e) {
+  numOfDivs = prompt("How many squares per side?");
+  numbOfDivsMultiplied = numOfDivs * numOfDivs - 1;
+  createMultipleDivs(numbOfDivsMultiplied);
+  return;
+}
+
+container.addEventListener("mouseover", mouseoverHandler);
+function mouseoverHandler(e) {
+  e.target.style.backgroundColor = "blue";
+}
+
+container.addEventListener("mouseout", mouseoutHandler);
+function mouseoutHandler(e) {
+  e.target.style.backgroundColor = "red";
+}
